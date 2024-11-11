3a54755ce82e21f298c2183a82a89122c088a42b
pechang03 <peter.shaw.cs@gmail.com>
2024-11-11 20:36:03 +0800

    feat: Add __all__ definition to expose public API in __init__.py

--- biclique_analysis/__init__.py
+++ biclique_analysis/__init__.py
@@ -4,6 +4,19 @@ from .classifier import classify_biclique, get_biclique_type_counts
 from .statistics import (
     calculate_biclique_statistics,
     calculate_coverage_statistics,
-    calculate_size_distribution
+    calculate_size_distribution,
 )
 from .reporting import print_bicliques_summary, print_bicliques_detail
+
+__all__ = [
+    "read_bicliques_file",
+    "process_bicliques",
+    "classify_biclique",
+    "get_biclique_type_counts",
+    "calculate_biclique_statistics",
+    "calculate_coverage_statistics",
+    "calculate_size_distribution",
+    "process_components",
+    "print_bicliques_summary",
+    "print_bicliques_detail",
+]
