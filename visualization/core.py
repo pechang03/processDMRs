d842411142ca3691b4e8718193df2cf73fab8ef3
pechang03 <peter.shaw.cs@gmail.com>
2024-11-13 14:33:00 +0800

    feat: Add additional edges for nodes without bicliques in visualization

--- visualization/core.py
+++ visualization/core.py
@@ -46,18 +46,24 @@ def create_biclique_visualization(
 
     traces = []
 
-    # 1. Add biclique boxes first (background)
-    box_traces = create_biclique_boxes(bicliques, node_positions, biclique_colors)
-    traces.extend(box_traces)
-
-    # 2. Add regular biclique edges
-    edge_traces = create_edge_traces(
-        bicliques, 
-        node_positions, 
-        edge_type="biclique",
-        edge_style={"color": "gray", "width": 1}
-    )
-    traces.extend(edge_traces)
+    # If we have the original graph, add edges for nodes without bicliques
+    if bipartite_graph is not None:
+        nodes_without_bicliques = set(bipartite_graph.nodes()) - set(
+            node_biclique_map.keys()
+        )
+        additional_edges = [
+            (u, v)
+            for u, v in bipartite_graph.edges()
+            if u in nodes_without_bicliques or v in nodes_without_bicliques
+        ]
+        if additional_edges:
+            edge_traces = create_edge_traces(
+                [(set([u]), set([v])) for u, v in additional_edges],
+                node_positions,
+                edge_type="non_biclique",
+                edge_style={"color": "lightgray", "width": 0.5, "dash": "dot"},
+            )
+            traces.extend(edge_traces)
 
     # 3. Add false positive edges if provided
     if false_positive_edges:
@@ -65,15 +71,19 @@ def create_biclique_visualization(
             [(set([n1]), set([n2])) for n1, n2 in false_positive_edges],
             node_positions,
             edge_type="false_positive",
-            edge_style={"color": "red", "width": 1, "dash": "dash"}
+            edge_style={"color": "red", "width": 1, "dash": "dash"},
         )
         traces.extend(fp_traces)
 
     # 4. Create NodeInfo object for node categorization
-    all_nodes = set().union(*[dmr_nodes | gene_nodes for dmr_nodes, gene_nodes in bicliques])
+    all_nodes = set().union(
+        *[dmr_nodes | gene_nodes for dmr_nodes, gene_nodes in bicliques]
+    )
     dmr_nodes = set().union(*[dmr_nodes for dmr_nodes, _ in bicliques])
     gene_nodes = all_nodes - dmr_nodes
-    split_genes = {node for node in gene_nodes if len(node_biclique_map.get(node, [])) > 1}
+    split_genes = {
+        node for node in gene_nodes if len(node_biclique_map.get(node, [])) > 1
+    }
     regular_genes = gene_nodes - split_genes
 
     node_info = NodeInfo(
@@ -82,17 +92,17 @@ def create_biclique_visualization(
         regular_genes=regular_genes,
         split_genes=split_genes,
         node_degrees={node: len(node_biclique_map.get(node, [])) for node in all_nodes},
-        min_gene_id=min(gene_nodes, default=0)
+        min_gene_id=min(gene_nodes, default=0),
     )
 
     # 5. Add nodes with proper styling
     node_traces = create_node_traces(
-        node_info, 
-        node_positions, 
-        node_labels, 
-        node_biclique_map, 
+        node_info,
+        node_positions,
+        node_labels,
+        node_biclique_map,
         biclique_colors,
-        dominating_set
+        dominating_set,
     )
     traces.extend(node_traces)
 
