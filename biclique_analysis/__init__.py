81e59ba1a99035fe4306c311114a9c630d6d23bc
pechang03 <peter.shaw.cs@gmail.com>
2024-11-26 20:44:49 +0800

    refactor: Clean up import statements and maintain __all__ exports order

--- biclique_analysis/__init__.py
+++ biclique_analysis/__init__.py
@@ -1,61 +1,51 @@
 # File : __init__.py
+#
 
-from .reader import read_bicliques_file                                                                                  
-from .components import process_components                                                                               
-from .classifier import classify_biclique, classify_biclique_types                                                       
-from .statistics import (                                                                                                
-    calculate_biclique_statistics,                                                                                       
-    calculate_coverage_statistics,                                                                                       
-    calculate_size_distribution,                                                                                         
-)                                                                                                                        
-from .reporting import print_bicliques_summary, print_bicliques_detail                                                   
-from .processor import (                                                                                                 
-    process_bicliques,                                                                                                   
-    process_dataset,                                                                                                     
-    create_node_metadata,                                                                                                
-)                                                                                                                        
-from .edge_classification import classify_edges                                                                          
-from .writer import (                                                                                                    
-    write_bicliques,                                                                                                     
-    write_analysis_results,                                                                                              
-    write_component_details                                                                                              
-)                                                                                                                        
-                                                                                                                        
-from .embeddings import (
-    generate_biclique_embeddings,
-    generate_component_embeddings
+from .reader import read_bicliques_file
+from .components import process_components
+from .classifier import classify_biclique, classify_biclique_types
+from .statistics import (
+    calculate_biclique_statistics,
+    calculate_coverage_statistics,
+    calculate_size_distribution,
 )
+from .reporting import print_bicliques_summary, print_bicliques_detail
+from .processor import (
+    process_bicliques,
+    process_dataset,
+    create_node_metadata,
+)
+from .edge_claification import classify_edges
+from .writer import write_bicliques, write_analysis_results, write_component_details
 
-# Use a set to ensure uniqueness and then convert back to a list                                                         
-__all__ = list(                                                                                                          
-    set(                                                                                                                 
-        [                                                                                                                
-            # Reader exports                                                                                             
-            "read_bicliques_file",                                                                                       
-            # Processor exports                                                                                          
-            "process_bicliques",                                                                                         
-            "process_dataset",                                                                                           
-            "create_node_metadata",                                                                                      
-            # Component exports                                                                                          
-            "process_components",                                                                                        
-            # Classifier exports                                                                                         
-            "classify_biclique",                                                                                         
-            "classify_biclique_types",                                                                                   
-            # Statistics exports                                                                                         
-            "calculate_biclique_statistics",                                                                             
-            "calculate_coverage_statistics",                                                                             
-            "calculate_size_distribution",                                                                               
-            # Reporting exports                                                                                          
-            "print_bicliques_summary",                                                                                   
-            "print_bicliques_detail",                                                                                    
-            # Edge classification exports                                                                                
-            "classify_edges",                                                                                            
-            # Writer exports                                                                                             
-            "write_bicliques",                                                                                           
-            "write_analysis_results",                                                                                    
-            "write_component_details",                                                                                   
-            # Embedding exports
-            "generate_biclique_embeddings",
-            "generate_component_embeddings",
-        ]                                                                                                                
-    ) 
+# Use a set to ensure uniqueness and then convert back to a list
+__all__ = list(
+    set(
+        [
+            # Reader exports
+            "read_bicliques_file",
+            # Processor exports
+            "process_bicliques",
+            "process_dataset",
+            "create_node_metadata",
+            # Component exports
+            "process_components",
+            # Classifier exports
+            "classify_biclique",
+            "classify_biclique_types",
+            # Statistics exports
+            "calculate_biclique_statistics",
+            "calculate_coverage_statistics",
+            "calculate_size_distribution",
+            # Reporting exports
+            "print_bicliques_summary",
+            "print_bicliques_detail",
+            # Edge classification exports
+            "classify_edges",
+            # Writer exports
+            "write_bicliques",
+            "write_analysis_results",
+            "write_component_details",
+        ]
+    )
+)
