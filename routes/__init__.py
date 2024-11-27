fd1612131ea013c291e0d5d6ff73536d9a52fae5
pechang03 <peter.shaw.cs@gmail.com>
2024-11-27 21:04:30 +0800

    deleted:    routes.py 	modified:   routes/__init__.py 	modified:   utils/__init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,31 +1,22 @@
-from flask import Blueprint
+"""Routes package initialization."""
 
-# Create blueprints for different route categories
-main_bp = Blueprint('main', __name__)
-stats_bp = Blueprint('stats', __name__)
-components_bp = Blueprint('components', __name__)
-
-# Import routes
-from .main_routes import *
-from .stats_routes import *
-from .component_routes import *
-
-# Register blueprints (this will be used in app.py)
-def register_blueprints(app):
-    app.register_blueprint(main_bp)
-    app.register_blueprint(stats_bp, url_prefix='/statistics')
-    app.register_blueprint(components_bp, url_prefix='/components')
 from flask import Blueprint
 
 # Create blueprints
-main_bp = Blueprint('main', __name__)
-components_bp = Blueprint('components', __name__, url_prefix='/components')
+main_bp = Blueprint("main", __name__)
+stats_bp = Blueprint("stats", __name__, url_prefix="/statistics")
+components_bp = Blueprint("components", __name__, url_prefix="/components")
 
-# Import routes
-from .main_routes import *
-from .component_routes import *
 
 def register_blueprints(app):
     """Register all blueprints with the app."""
+    # Register blueprints
     app.register_blueprint(main_bp)
+    app.register_blueprint(stats_bp)
     app.register_blueprint(components_bp)
+
+
+# Import routes after blueprint creation to avoid circular imports
+from . import main_routes
+from . import component_routes
+from . import stats_routes
